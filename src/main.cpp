a86f3c2b76f70b3a3138f79cfbd14b93c798d2af
militch <litch.me@gmail.com>
2020-12-19 08:18:47 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,69 @@
 #include <string>
 #include <chrono>
 #include <climits>
+#include <stdexcept>
 #include "util/sha256.h"
+#ifdef ISWIN
+#include "win32/wingetopt.h"
+#endif
 
-#define DIFFICULTY 5
-std::string prepareData(){
+static struct option const long_options[] = {
+    {"difficulty", required_argument, NULL, 'd'}
+};
+
+void usage(){
+    fprintf(stdout,
+            "Usage: powtest [options...] <message>\n"
+            "\n"
+            "Options:\n"
+            "  -d <number> POW difficulty of number\n"
+            "  -v    Print package version\n"
+            "  -h    Show version number and quit\n");
+}
+
+std::string prepareData(int len){
     std::string t;
-    for (int i=0; i<DIFFICULTY; i++) {
+    for (int i=0; i<len; i++) {
         t += "0";
     }
     return t;
 }
-
-int main(int, char**) {
+int main(int argc, char **argv) {
+    int opt;
+    int difficulty_num;
+    while ((opt = getopt(argc, argv, "d:vh")) != EOF) {
+        switch (opt) {
+            case 'd':
+                try {
+                    difficulty_num = std::stoi(std::string(optarg));
+                }catch (std::invalid_argument iaex) {
+                    usage();
+                    return 0;
+                }
+                break;
+            case 'h':
+            default:
+                usage();
+                return 0;
+        }
+    }
+    int argind = optind;
+    std:: string message;
+    if (argind < argc) {
+        message = std::string(argv[argind]);
+    }
+    if (message.empty()){
+        usage();
+        return 0;
+    }
     long nonce = 0;
     std::chrono::milliseconds beginMs = std::chrono::duration_cast< std::chrono::milliseconds >(
         std::chrono::system_clock::now().time_since_epoch()
     );
-    std::string msg = "hello, world";
     std::cout << "test begin time: " <<  beginMs.count() << std::endl;
     while (nonce < LONG_MAX) {
-        std::string hash = sha256(msg + std::to_string(nonce));
-        std::string target = prepareData();
+        std::string hash = sha256(message + std::to_string(nonce));
+        std::string target = prepareData(difficulty_num);
         std::string c = hash.substr(0,target.length());
         // std::cout << "hash: " << hash << ", target: " << target << ", nonce: " << nonce << std::endl;
         if (target == c) {
